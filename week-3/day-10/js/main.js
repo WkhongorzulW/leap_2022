af0c8120ab01617ce2bb2d8aa71214261f56f55f
Morning <morning@admins-iMac.local>
2022-11-28 12:48:15 +0800

    day-10 afternoon-1

---
+++ week-3/day-10/js/main.js
@@ -0,0 +1,62 @@
+console.log("Hello World from Javascript");
+console.log("My first JS tutorial");
+console.log("In a relationship with coding <3");
+// alert("Hello from JS world to the Alert!");
+
+var hello ;
+console.log(hello);
+
+hello = "Hello World from Javascript!!!";
+console.log(hello);
+
+hello = "Here is now apple!!!";
+console.log(hello);
+
+let myNumber;
+console.log(myNumber);
+
+myNumber = 5;
+console.log(myNumber);
+
+let x = 8, y = 6, z = 18;
+
+console.log(x + y);
+
+console.log(x + y + 15);
+
+console.log(x * x + y * y);
+
+
+const PI = 3.14;
+
+console.log(PI);
+
+
+var a = 5;
+var a = 6;
+
+console.log(a);
+
+
+let b = 6;
+c = 8;
+console.log(c);
+
+console.log(b);
+
+let message = 'HI';
+console.log(message);
+
+console.log(message + b + c);
+
+message = 8;
+console.log(message);
+
+let mymessage = 'HI I added ';
+let added = 'It is ';
+
+var d = 6;
+var e = 8;
+
+alert ("HI I added 6 + 8");
+alert (added + [d + e]);
