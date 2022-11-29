405c1d76bec9c3dce0d1260c2e5676fc2b5e6f73
Morning <morning@admins-iMac.local>
2022-11-29 12:35:18 +0800

    day11 afternoon-1

---
+++ week-3/day-11/js/main.js
@@ -0,0 +1,183 @@
+// var a = 6;
+// console.log(a);
+
+// let b = "Baby shark";
+// let c = ", doo-doo";
+// let d = "Mommy shark";
+
+// console.log(b + c + c);
+// console.log(b + c + c);
+// console.log(b + c + c);
+// console.log(b);
+
+// console.log(d + c + c);
+// console.log(d + c + c);
+// console.log(d + c + c);
+// console.log(d);
+
+// let myNumber = 5;
+// let undify;
+// let nullVariable = null;
+// let myDesision = true;
+
+// //typeof
+// console.log(typeof 1);
+// console.log(typeof "Hello");
+// console.log(typeof undify);
+// console.log(typeof nullVariable);
+// console.log(myDesision);
+// console.log(typeof myDesision);
+
+/*
+ * alert agrgana
+ *
+ * x'2 + y'2 - z'3 / 240 * 175
+*/
+
+// let x = 8, y = 5, z = 1, q = 240, w = 175;
+// //alert(x * x + y * y - z * z * z / q * w);
+
+// console.log(7 % 2);
+// console.log(36 % 19);
+
+// let result = '30' - 10;
+// console.log(result);
+
+// 1. toirgiin urt 
+//const PI = 3.14;
+//let r = 4;
+//alert(2 * PI * 4);
+
+// 2. trapeziin S
+//let k = 4, l = 8, h = 4;
+//alert([k + l] / 2 * h);
+
+//let test = prompt('Please give me your top');
+//let bottom = prompt('Please give me your bottom');
+//let height = prompt('Please give me your height');
+
+//alert([test + bottom] / 2 * height);
+
+// let a = prompt("give me number 'a'"), b = prompt("give me number 'b'");
+
+// if(a > b){
+//     console.log("it's true")
+// } else{
+//     console.log("it's false")
+// }
+
+
+// let yearOfBirth = prompt ("Please give me your birth year");
+// const currentYear = 2022;
+// const old = currentYear - yearOfBirth;
+// if(0 <= old && old <= 1) {
+//     console.log("Infant")
+// } 
+// else if(1 < old && old <= 3){
+//     console.log("Toddler")
+// } 
+// else if(3 < old && old <= 5) {
+//     console.log("Preschool")
+// } 
+// else if(5 < old && old <= 12) {
+//     console.log("Gradeschooler")
+// } 
+// else if(12 < old && old <= 18) {
+//     console.log("Teen")
+// }
+// else if(18 < old && old <= 21) {
+//     console.log("Young adult")
+// } 
+// else {
+//     console.log("Adult")
+// }
+
+
+// let a = prompt ("give number a");
+// let result = a % 2;
+// let correct = "Even number";
+// let incorrect = "Odd number";
+// if (result == 0){
+//     console.log(correct)
+// }
+// else {
+//     console.log(incorrect)
+// }
+
+
+
+
+// let c = prompt ("give number c");
+// let d = c % 3;
+// let e = c % 7;
+// if(d == 0 || e == 0){
+//     console.log("Yes")
+// }
+// else{
+//     console.log("No")
+// }
+
+
+// let weight = prompt ("Please give me your weight by 'kg'");
+// let height = prompt ("Please give me your height by 'm'");
+// let bmi = weight / [height * height];
+// if (bmi < 18.5){
+//     console.log("Under Weight")
+// }
+// else if (18.5 < bmi && bmi <= 24.9){
+//     console.log("Normal")
+// }
+// else if (25 < bmi && bmi <= 29.9){
+//     console.log("Over Weight")
+// }
+// else if (30 < bmi && bmi <= 34.9){
+//     console.log("Obesity (Class 1)")
+// }
+// else if (35 < bmi && bmi <= 40){
+//     console.log("Obesity (Class 2)")
+// }
+// else if (40 < bmi){
+//     console.log("Extreme Obesity")
+// }
+
+
+// let day = prompt ("Give a week day number");
+// let dayNumber = Number(day);
+// console.log(typeof dayNumber)
+// let dayName;
+// switch(dayNumber){
+//     case 1: 
+//         dayName = 'Monday';
+//         break;
+//     case 2:
+//         dayName = 'Tuesday';
+//         break;
+//     case 3:
+//         dayName = 'Wednesday';
+//         break;
+//     case 4:
+//         dayName = 'Thursday';
+//         break;
+//     case 5:
+//         dayName = 'Friday';
+//         break;
+//     case 6: 
+//         dayName = 'Saturday';
+//         break;
+//     case 7:
+//         dayName = 'Sunday';
+//         break;
+//     default:
+//         dayName = 'Invalid day'
+// }
+// console.log(dayName);
+// console.log(typeof day);
+
+
+let b = prompt ("give number b");
+if (b > 0){
+    console.log("Positive")
+}
+else {
+    console.log("Negative")
+}
