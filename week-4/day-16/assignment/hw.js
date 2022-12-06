e6d3d13cc0879fe07e263eb1265c04c216dce305
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-06 23:17:53 +0800

    day16 evening-3

--- week-4/day-16/assignment/hw.js
+++ week-4/day-16/assignment/hw.js
@@ -20,14 +20,51 @@
 
 
 
-// Avarage of array
-let findOutAvarage = [1, 2, 3, 4, 5, 6];
-let sum = 0;
-function f2(x){
-    for(; x < findOutAvarage.length; x++){
-        sum = sum + findOutAvarage[x];
-    }
-    console.log(sum);
-}
-f2(0);
+// // Avarage of array
+// let findOutAvarage = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+// function f2(x){
+//     for(; x < findOutAvarage.length; x++){
+//         sum = sum + findOutAvarage[x];
+//     }
+//     console.log(sum / 2);
+// }
+// f2(0);
 
+
+
+// // Sum of array
+// let array1 = [1, 2, 3, 4, 5, 6];
+// let sum1 = 0;
+// function f3(x){
+//     for(; x < array1.length; x++){
+//         sum1 = sum1 + array1[x];
+//     }
+//     console.log(sum1);
+// }
+// f3(0);
+
+
+
+// // Even or odd
+// let guess = Number(prompt("Give a prime number from 0 to 50"));
+// let temp = 0;
+// function f4(x){
+//     debugger
+//     for(let a = 2; a < guess; a++){
+//         if(x % a == 0){
+//             temp = temp + 1;
+//         }
+//     }
+//     if(temp == 0){
+//         console.log("It's prime number");
+//     } else{
+//         console.log("It isn't prime number");
+//     }
+// }
+// f4(guess);
+
+
+//2D Array
+let row = Number(prompt("Give me your row"));
+let col = Number(prompt("Give me your column"));
