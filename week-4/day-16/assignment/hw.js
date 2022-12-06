fa387706d2f04c2d7933d7217fd3148dc7d7221c
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-06 22:39:39 +0800

    day16 evening-1

--- week-4/day-16/assignment/hw.js
+++ week-4/day-16/assignment/hw.js
@@ -1,13 +1,33 @@
-let numbers = [1, 2, 3, 12, 34, 45, 67, 89, 90, 100];
-let guess = Number(prompt("Give a random number"));
-function f1(x){
-    for(let i = 0; i < numbers[i]; i++){
-        if(numbers[i] == x){
-            console.log(`${guess} is here`);
-        }
-        else{
-            console.log(`${guess} isn't here`);
-        }
+// // Guess the number of array 
+// let numbers = [1, 2, 3, 11, 12, 13, 21, 22, 23];
+// let guess = Number(prompt("Give a random number"));
+
+// function f1(x){
+//     for(let i = 0; i < numbers.length; i++){
+//         if(numbers[i] == x){
+//             console.log(`${guess} is here!`)
+//             return true;
+//         }
+//     }
+//     for(let o = 0; o < numbers.length; o++){
+//         if(numbers[o] != x){
+//             console.log(`${guess} isn't here!`)
+//             return false;
+//         }
+//     }
+// }
+// console.log(f1(guess));
+
+
+
+// Avarage of array
+let findOutAvarage = [1, 2, 3, 4, 5, 6];
+let sum = 0;
+function f2(x){
+    for(; x < findOutAvarage.length; x++){
+        sum = sum + findOutAvarage[x];
     }
+    console.log(sum);
 }
-console.log(f1(numbers[i]));
+f2(0);
+
