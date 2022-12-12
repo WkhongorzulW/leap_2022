d4ba9983f17eb06509f5045827f21ac58cc80309
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-12 23:08:16 +0800

    day20 evening-2

--- JS-DOM/JS DOM assignments/ex-02/js/main.js
+++ JS-DOM/JS DOM assignments/ex-02/js/main.js
@@ -1,11 +1,11 @@
 let myButton = document.querySelector("input");
 
-let remove = document.querySelector("option");
+let element = document.querySelector("option");
+console.log(element);
 
 console.log(myButton);
 
-// myButton.addEventListener("click", () => {
-//     remove.appendChild(remove)[0];
-// })
-
-remove.removeChild();
+function removing(){
+    element.remove();
+}
+myButton.addEventListener("click", removing);
