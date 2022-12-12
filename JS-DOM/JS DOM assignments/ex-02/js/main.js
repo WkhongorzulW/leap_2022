17168225e951ecb7c773ad18fd74c0563596f95a
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-12 20:39:11 +0800

    day20  evening

---
+++ JS-DOM/JS DOM assignments/ex-02/js/main.js
@@ -0,0 +1,11 @@
+let myButton = document.querySelector("input");
+
+let remove = document.querySelector("option");
+
+console.log(myButton);
+
+// myButton.addEventListener("click", () => {
+//     remove.appendChild(remove)[0];
+// })
+
+remove.removeChild();
