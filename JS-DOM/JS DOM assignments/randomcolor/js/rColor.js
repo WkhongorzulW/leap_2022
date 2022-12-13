acbbc42ea7566a47d619451a7669e72b5807f046
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-13 13:34:04 +0800

    day21

--- JS-DOM/JS DOM assignments/randomcolor/js/rColor.js
+++ JS-DOM/JS DOM assignments/randomcolor/js/rColor.js
@@ -1,5 +1,9 @@
 const button = document.querySelector("#rColorBtn");
-function changingColor(){
-    document.querySelector("body").style = "background-color: ";
-}
-button.addEventListener("click", changingColor);
+let body = document.querySelector("body");
+let colors = ["red", "blue", "pink", "skyblue", "white", "lihgtpink", "black", "green", "yellow", "lightblue", "purple", "pale", "salmon", "gold"];
+console.log(colors[0]);
+button.addEventListener("click", () => {
+    for(let i = 0; i < colors.length; i++){
+        body.style = `background-color: ${colors[i]}`
+    }
+}, 3000);
