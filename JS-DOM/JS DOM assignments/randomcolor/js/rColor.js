a1c44e7a85a6b70acedbfc50ab7a8528bb3ef839
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-13 21:53:55 +0800

    day21 evening-1

--- JS-DOM/JS DOM assignments/randomcolor/js/rColor.js
+++ JS-DOM/JS DOM assignments/randomcolor/js/rColor.js
@@ -1,9 +1,47 @@
 const button = document.querySelector("#rColorBtn");
+button.style = "color: green";
 let body = document.querySelector("body");
-let colors = ["red", "blue", "pink", "skyblue", "white", "lihgtpink", "black", "green", "yellow", "lightblue", "purple", "pale", "salmon", "gold"];
-console.log(colors[0]);
+let color = "white";
 button.addEventListener("click", () => {
-    for(let i = 0; i < colors.length; i++){
-        body.style = `background-color: ${colors[i]}`
+    if(color == "white"){
+        color = "green";
+        body.style = "background-color: green"
+        button.style = "color: red";
+    } else if(color == "green"){
+        color = "red";
+        body.style = "background-color: red";
+        button.style = "color: lightblue";
+    } else if(color == "red"){
+        color = "lightblue";
+        body.style = "background-color: lightblue";
+        button.style = "color: pink";
+    } else if(color == "lightblue"){
+        color = "pink";
+        body.style = "background-color: pink";
+        button.style = "color: yellow";
+    } else if(color == "pink"){
+        color = "yellow";
+        body.style = "background-color: yellow";
+        button.style = "color: salmon";
+    } else if(color == "yellow"){
+        color = "salmon";
+        body.style = "background-color: salmon";
+        button.style = "color: palevioletred";
+    } else if(color == "salmon"){
+        color = "palevioletred";
+        body.style = "background-color: palevioletred";
+        button.style = "color: voilet";
+    } else if(color == "palevioletred"){
+        color = "lightgreen";
+        body.style = "background-color: lightgreen";
+        button.style = "color: gold";
+    } else if(color == "lightgreen"){
+        color = "gold";
+        body.style = "background-color: gold";
+        button.style = "color: black";
+    } else if(color == "gold"){
+        color = "white";
+        body.style = "background-color: white";
+        button.style = "color: green";
     }
-}, 3000);
+});
