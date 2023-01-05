d6d71e7096cf6334179921124a3fbdf605d4a389
WkhongorzulW <bkhongorzul064@gmail.com>
2023-01-06 00:18:14 +0800

    some exercises added

---
+++ JS/JS assignments/JS function/tegsh-sondgoi-index.html/main.js
@@ -0,0 +1,14 @@
+function twoNumbers(a, b) {
+    let evenNumbers = [];
+    for(let i = a; i < b; i++){
+        if(a % 2 === 0){
+            console.log(a);
+            console.log(evenNumbers);
+            evenNumbers.appendChild(1);
+        }
+    }
+    return evenNumbers;
+}
+
+twoNumbers(1, 10);
+console.log(twoNumbers());
